338ce220678ed4ed566907b7b117c3e58ce44e6a
iulian-mocanu <82028104+iulian-mocanu@users.noreply.github.com>
2023-07-27 21:38:53 +1000

    added extra steps for test no.2

--- cypress/e2e/content.cy.ts
+++ cypress/e2e/content.cy.ts
@@ -12,14 +12,38 @@ describe('Page Content', () => {
 
   it('Should display expected values for race row contents', () => {
     // Race number, venue name, time to jump
-    cy.intercept('GET', 'http://localhost:3000/v2/racing/next-races-category-group?count=5&categories=%5B%224a2788f8-e825-4d36-9894-efd4baf1cfae%22%2C%229daef0d7-bf3c-4f50-921d-8e818c60fe61%22%2C%22161d9be2-e909-4326-8c2c-35ed71fb460b%22%5D', { fixtures: 'get-races' }).as('getData');
+    cy.intercept('GET', 'http://localhost:3000/v2/racing/next-races-category-group?count=5&categories=%5B%224a2788f8-e825-4d36-9894-efd4baf1cfae%22%2C%229daef0d7-bf3c-4f50-921d-8e818c60fe61%22%2C%22161d9be2-e909-4326-8c2c-35ed71fb460b%22%5D', 
+    { fixture: 'get-races.json'  }).as('getData');
     cy.visit('http://localhost:3000/'); 
-    cy.wait('@getData')
-    //Sanity check
-    .its('response.statusCode').should('eq', 200);
-    //Unable to move beyond this error: > Cannot convert undefined or null to object
+    cy.wait('@getData').its('response.statusCode').should('eq', 200);
 
+    //1.Identify the top 5 earliest races based on "advertised_start"
+    //1.a Get the race summaries from the JSON data
+    cy.fixture('get-races.json').then((data) => {
+      const raceSummaries = data.race_summaries;
+
+      // 1.b Sort the race summaries based on the earliest advertised start time
+      const sortedRaceSummaries = Object.values(raceSummaries).sort((a, b) =>
+        new Date(a.advertised_start) - new Date(b.advertised_start)
+      );
+
+      // 1.c Get the top 5 races with the earliest advertised start
+      const top5Races = sortedRaceSummaries.slice(0, 5);
+
+      // 1.d Extract the meeting name and race number for each of the top 5 races
+      const earliestRaceNumbers = top5Races.map((race) => race.race_number);
+
+      // 1.e Log the results for verification
+      cy.log(top5Races);
+
+      // 2. Perform the assertions for each race number
+      cy.get(':nth-child(1) > .race-name > .race-number').should('contain', earliestRaceNumbers[0]);
+      cy.get(':nth-child(2) > .race-name > .race-number').should('contain', earliestRaceNumbers[1]);
+      cy.get(':nth-child(3) > .race-name > .race-number').should('contain', earliestRaceNumbers[2]);
+      cy.get(':nth-child(4) > .race-name > .race-number').should('contain', earliestRaceNumbers[3]);
+      cy.get(':nth-child(5) > .race-name > .race-number').should('contain', earliestRaceNumbers[4]);
     });
+  });
 
   it('Insert additional tests here and below', () => {
     
